a551caee4767558635019b65a9b05a012b7734d1
haipink998 <tahonghai1998@gmail.com>
2021-03-19 11:33:42 +0700

    'add-news'

--- pages/news.vue
+++ pages/news.vue
@@ -1,60 +1,78 @@
 <template>
-<div class="content">
-  <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-    <div slot="footer"><b>ant design vue</b> footer part</div>
-    <a-list-item slot="renderItem" key="item.title" slot-scope="item">
-      <template v-for="{ type, text } in actions" slot="actions">
-        <span :key="type">
-          <a-icon :type="type" style="margin-right: 8px" />
-          {{ text }}
-        </span>
-      </template>
-      <img
-        slot="extra"
-        width="272"
-        alt="logo"
-        src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-      />
-      <a-list-item-meta :description="item.description">
-        <a slot="title" :href="item.href">{{ item.title }}</a>
-        <a-avatar slot="avatar" :src="item.avatar" />
-      </a-list-item-meta>
-      {{ item.content }}
-    </a-list-item>
-  </a-list>
-</div>
+  <div class="content">
+    <a-list
+      item-layout="vertical"
+      size="large"
+      :pagination="pagination"
+      :data-source="listNews"
+    >
+      <a-list-item slot="renderItem" key="item.title" slot-scope="item">
+        <template v-for="{ type, text } in actions" slot="actions">
+          <span :key="type">
+            <a-icon :type="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+        </template>
+        <img
+          slot="extra"
+          width="272"
+          alt="logo"
+          src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+        />
+        <a-list-item-meta :description="item.description">
+          <a slot="title" :href="item.href">{{ item.title }}</a>
+          <a-avatar slot="avatar" :src="item.attach_file" />
+        </a-list-item-meta>
+        {{ item.content }}
+      </a-list-item>
+    </a-list>
+  </div>
 </template>
 <script>
+import mutationTypes from '~/constants/mutationTypes';
 const listData = [];
 for (let i = 0; i < 23; i++) {
   listData.push({
-    href: 'https://www.antdv.com/',
+    href: "https://www.antdv.com/",
     title: `ant design vue part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    avatar: "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png",
     description:
-      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+      "Ant Design, a design language for background applications, is refined by Ant UED Team.",
     content:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently."
   });
 }
-
+import { convertTime } from "~/helpers/time";
 export default {
+  mounted() {
+    this.$store.dispatch(`news/${mutationTypes.USER.GET_NEWS}`);
+  },
+  computed: {
+    listNews: function(){
+      return (this.$store.getters["news/newsList"] || []).map(el => ({
+        ...el,
+        created_at: el.created_at ? convertTime(el.created_at) : 'Processing',
+      }))
+    },
+    imageNews: function(){
+      return (this.$store.getters["news/newsList"]?.attach_file || null)
+    }
+  },
   data() {
     return {
-      listData,
       pagination: {
         onChange: page => {
           console.log(page);
         },
-        pageSize: 3,
+        pageSize: 3
       },
       actions: [
-        { type: 'star-o', text: '156' },
-        { type: 'like-o', text: '156' },
-        { type: 'message', text: '2' },
-      ],
+        { type: "star-o", text: "156" },
+        { type: "like-o", text: "156" },
+        { type: "message", text: "2" }
+      ]
     };
-  },
+  }
 };
 </script>
 <style></style>
